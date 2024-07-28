95a55e6683f745adad0b26aa2c9573047ba82994
prj2032oleg <prj2032.04lst@gmail.com>
2024-07-28 13:17:20 +0200

    (+) freiburg teams Vladislav Wolpert

--- assets/js/Freiburg-Herdern.js
+++ assets/js/Freiburg-Herdern.js
@@ -142,10 +142,21 @@ const Freiburg = [{
         "title": {
           "de": "Manfred Sauer",
           "ru": "Manfred Sauer"
-        },        
+        },
         "designation": {
           "de": "Zahnarzt",
           "ru": "Zahnarzt"
+        }
+      },
+      {
+        "src": "assets/images/tkacheva/team/v3/WolpertVladi.png",
+        "title": {
+          "de": "Vladislav Wolpert",
+          "ru": "Vladislav Wolpert"
+        },        
+        "designation": {
+          "de": "Vorbereitungsassistent",
+          "ru": "Vorbereitungsassistent"
         }
       },
       {
